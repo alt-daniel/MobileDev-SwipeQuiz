3eb14e8c09116475722c11a78d9ede9e17fba60a
D.P.N.P. Altorf <daniel.altorf@hva.nl>
2019-10-10 13:50:35 +0200

    Adds recyclerview with questions.

--- app/src/main/java/com/example/swipequiz/MainActivity.kt
+++ app/src/main/java/com/example/swipequiz/MainActivity.kt
@@ -2,11 +2,33 @@ package com.example.swipequiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val questions = ArrayList<Question>()
+    private val questionAdapter = QuestionAdapter(questions)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initView()
+    }
+
+    private fun initView() {
+        rvQuestions.layoutManager = LinearLayoutManager(this@MainActivity, RecyclerView.VERTICAL, false)
+        rvQuestions.adapter = questionAdapter
+        rvQuestions.addItemDecoration(DividerItemDecoration(this@MainActivity, DividerItemDecoration.VERTICAL))
+
+        for (i in Question.QUESTIONS.indices) {
+            questions.add(Question(Question.QUESTIONS[i], Question.ANSWER[i]))
+        }
+
+        questionAdapter.notifyDataSetChanged()
+
     }
 }
---
+++ app/src/main/java/com/example/swipequiz/QuestionAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.swipequiz
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class QuestionAdapter (private val questions: List<Question>): RecyclerView.Adapter<QuestionAdapter.ViewHolder>() {
+
+    lateinit var context: Context
+
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(questions[position])
+    }
+
+    override fun getItemCount(): Int {
+        return questions.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        context = parent.context
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(android.R.layout.simple_list_item_1, parent, false)
+        )
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        private val tvQuestion: TextView = itemView.findViewById(android.R.id.text1)
+
+        fun bind(question: Question) {
+            tvQuestion.text = question.name
+        }
+    }
+}
